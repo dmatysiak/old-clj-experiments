a71fb758cf21f68f788c0e81dca8607a6daa2157
Daniel Matysiak <daniel.matysiak@l2inc.com>
2016-02-11 19:49:24 -0500

    Back it up

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://github.com/dmatysiak/organon.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [net.mikera/core.matrix "0.49.0"]]
   :java-source-paths ["src/organon/java"]
-  :main ^:skip-aot organon.core
+  ;;:main ^:skip-aot organon.core
   :target-path "target/%s")
--- src/organon/core.clj
+++ src/organon/core.clj
@@ -1,6 +1,52 @@
 (ns organon.core)
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+
+;;
+;; Protocols
+;;
+(defprotocol Functor
+  (fmap [ftr fn])
+  (<$ [ftr obj]))
+
+(defprotocol Applicative
+  (pure [ftr fn])
+  (<*> [ftr obj])
+  (*> [ftr obj])
+  (<* [ftr obj]))
+
+
+;;
+;; Records
+;;
+(defrecord Tree [value children])
+
+
+;;
+;; Protocol Implementations
+;;
+(extend-protocol Functor
+  Tree
+  (fmap [ftr fn]
+    (Tree. (fn (:value ftr))
+           (let [children (:children ftr)]
+             (if (some? children)
+               (map #(fmap % fn) children)))))
+  (<$ [ftr obj]
+    (Tree. obj
+           (let [children (:children ftr)]
+             (if (some? children)
+               (map #(<$ % obj) children))))))
+
+(extend-protocol Functor
+  clojure.lang.PersistentVector
+  (fmap [ftr fn]
+    (vec (map fn ftr)))
+  (<$ [ftr obj]
+    (vec (repeat (count ftr) obj))))
+
+(extend-protocol Functor
+  clojure.lang.PersistentList
+  (fmap [ftr fn]
+    (map fn ftr))
+  (<$ [ftr obj]
+    (repeat (count ftr) obj)))
